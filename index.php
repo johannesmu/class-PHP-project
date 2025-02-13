c3f9c7aedc9dffc938688c278a9f6bd37cbf694d
Johannes <jmuljana@gmail.com>
2025-02-13 00:47:56 +0000

    added twig and templates

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php 
-include "header.php";
-echo "<h1 style='color:red;'>Hello World Again, yeah</h1>";
-include "footer.php";
+require_once 'vendor/autoload.php';
+// create data variables
+$page_title = "Class Project Home Page";
+$greeting = "Welcome to my website";
+// Loading the twig template
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment( $loader );
+$template = $twig -> load( 'page.twig' );
+// render the ouput
+echo $template -> render( [ 'title' => $page_title, 'greeting' => $greeting ] );
 ?>
