f3a75f671266870bf62dfe9f6d174af9f2524af2
Johannes <jmuljana@gmail.com>
2025-02-19 22:37:18 +0000

    added .env file

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # class-PHP-project
 A php sample project for **Advanced Web** class 2025
-                            
+
+# Configuration
+rename *.env.dist* to *.env* and change values to suit your environment                         
 
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php 
 require_once 'vendor/autoload.php';
+// testing phpdotenv
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 // create data variables
 $page_title = "Class Project Home Page";
 $greeting = "Welcome to my website";
