90cbd456d100f5a8394059e67c0ed264ec45dd41
Johannes <jmuljana@gmail.com>
2025-02-20 00:15:47 +0000

    updated App class

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php 
 require_once 'vendor/autoload.php';
-// testing phpdotenv
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+// classes used in this page
+use Johannes\Classproject\App;
+
+// create app from App class
+$app = new App();
+$site_name = $app -> site_name;
 // create data variables
 $page_title = "Class Project Home Page";
 $greeting = "Welcome to my website";
@@ -11,5 +14,9 @@ $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment( $loader );
 $template = $twig -> load( 'page.twig' );
 // render the ouput
-echo $template -> render( [ 'title' => $page_title, 'greeting' => $greeting ] );
+echo $template -> render( [ 
+    'title' => $page_title, 
+    'greeting' => $greeting,
+    'website_name' => $site_name 
+] );
 ?>
---
+++ src/App.php
@@ -0,0 +1,31 @@
+<?php
+namespace Johannes\Classproject;
+use Dotenv\Dotenv;
+use \Exception;
+
+class App {
+    protected $config;
+    public $site_name;
+    
+    public function __construct()
+    {
+        // class constructor
+        $this -> loadConfig();
+    }
+
+    private function loadConfig()
+    {
+        try {
+            // cwd = current working directory
+            $app_dir = getcwd();
+            $dotenv = Dotenv::createImmutable($app_dir);
+            $dotenv->load();
+            $this -> site_name = $_ENV['SITENAME'];
+        }
+        catch ( Exception $exception) {
+            $msg = $exception -> getMessage();
+            exit($msg);
+        }
+    }
+}
+?>
