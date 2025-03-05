3553ac3b9dedd7b10e6d3c1bc1acd8eb8fc8faee
Johannes <jmuljana@gmail.com>
2025-03-05 22:48:15 +0000

    signup template

--- signup.php
+++ signup.php
@@ -1,3 +1,21 @@
-<?php
-echo "Signup";
+<?php 
+require_once 'vendor/autoload.php';
+// classes used in this page
+use Johannes\Classproject\App;
+
+// create app from App class
+$app = new App();
+$site_name = $app -> site_name;
+// create data variables
+$page_title = "Signup for an account";
+
+// Loading the twig template
+$loader = new \Twig\Loader\FilesystemLoader('templates');
+$twig = new \Twig\Environment( $loader );
+$template = $twig -> load( 'signup.twig' );
+// render the ouput
+echo $template -> render( [ 
+    'title' => $page_title, 
+    'website_name' => $site_name 
+] );
 ?>
